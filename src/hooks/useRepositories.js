60dc78b77bcc56da28bce329fd20d96eae898d0a
Niko Raulas <niko.raulas@outlook.com>
2025-03-03 19:03:43 +0200

    10.11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  return { repositories: data?.repositories || null, loading, refetch, error };
+};
+
+export default useRepositories;
